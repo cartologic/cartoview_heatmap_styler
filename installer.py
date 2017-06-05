9a901cd11bbbe812a2e4555b3a6959687ae9c790
kamalseisy <kamal.alseisy@cartologic.com>
2017-06-05 13:37:51 +0200

    Initial commit

---
+++ installer.py
@@ -0,0 +1,21 @@
+__author__ = 'cartologic'
+
+info = {
+    "title": "Heatmap Styler",
+    "description": "Create Heatmap styles for WMS layers with cartoview",
+    "author": 'Cartologic',
+    "home_page": 'http://cartoview.org/apps/cartoview_heatmap_styler',
+    "help_url": "http://cartoview.org/apps/cartoview_heatmap_styler/help/",
+    "tags": ['app', 'map', 'styler', 'sld', 'Heatmap'],
+    "licence": 'BSD-2',
+    "author_website": "http://cartologic.com",
+    "single_instance": True
+}
+
+
+def install():
+    pass
+
+
+def uninstall():
+    pass
